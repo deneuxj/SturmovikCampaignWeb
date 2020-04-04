a9bb364b1eaa70cc439f51026533b71b332ef7d8
Johann Deneux <johann.deneux@gmail.com>
2020-04-04 11:46:09 +0200

    Show details of commands and results

--- client.ts
+++ client.ts
@@ -92,29 +92,238 @@ interface WarState {
     BuildingHealth: BuildingStatus[]
 }
 
-type ValueType = number | string | object | Dict<number | string | object>
-
-interface Command {
-    Verb: string
-    Args: Record<string, ValueType>
+interface DamageBuildingPartArgs {
+    BuildingAt: OrientedPosition
+    Part: number
+    Damage: number
 }
+
+interface DamageBuildingPartCmd {
+    Verb: "DamageBuildingPart"
+    Args: DamageBuildingPartArgs
+}
+
+interface RepairBuildingPartArgs {
+    BuildingAt: OrientedPosition
+    Part: number
+    Repair: number
+}
+
+interface RepairBuildingPartCmd {
+    Verb: "RepairBuildingPart"
+    Args: RepairBuildingPartArgs
+}
+
+interface AddRemovePlaneArgs {
+    Airfield: string
+    Plane: string
+    Amount: number
+}
+
+interface AddRemovePlaneCmd {
+    Verb: "RemovePlane" | "AddPlane"
+    Args: AddRemovePlaneArgs
+}
+
+interface AddDestroyGroundForcesArgs {
+    Region: string
+    Coalition: Coalition
+    Amount: number
+}
+
+interface AddDestroyGroundForcesCmd {
+    Verb: "AddGroundForces" | "DestroyGroundForces"
+    Args: AddDestroyGroundForcesArgs
+}
+
+interface MoveGroundForcesArgs {
+    Start: string
+    Destination: string
+    Coalition: Coalition
+    Amount: number
+}
+
+interface MoveGroundForcesCmd {
+    Verb: "MoveGroundForces"
+    Args: MoveGroundForcesArgs
+}
+
+interface SetRegionOwnerArgs {
+    Region: string
+    Coalition: Coalition
+}
+
+interface SetRegionOwnerCmd {
+    Verb: "SetRegionOwner"
+    Args: SetRegionOwnerArgs
+}
+
+interface AdvanceTimeArgs {
+    Hours: number
+}
+
+interface AdvanceTimeCmd {
+    Verb: "AdvanceTime"
+    Args: AdvanceTimeArgs
+}
+
+interface OtherCmd {
+    Verb: string
+    Args: any
+}
+
+type Command = DamageBuildingPartCmd | RepairBuildingPartCmd | AddRemovePlaneCmd | AddDestroyGroundForcesCmd | MoveGroundForcesCmd | SetRegionOwnerCmd | AdvanceTimeCmd | OtherCmd
 
 function commandToHtml(cmd: Command) {
+    let description = undefined
+    let args = undefined
+    function astxt(s: string) { return document.createTextNode(s) }
+    switch (cmd.Verb) {
+        case "AddGroundForces":
+            description = "Add ground forces"
+            args = astxt(`In ${cmd.Args.Region}, for ${cmd.Args.Coalition}: ${cmd.Args.Amount}`)
+            break
+        case "DamageBuildingPart":
+            description = "Damage building"
+            args = astxt(`At ${cmd.Args.BuildingAt.Position.X}, ${cmd.Args.BuildingAt.Position.Y}, part ${cmd.Args.Part}: ${cmd.Args.Damage}`)
+            break
+        case "DestroyGroundForces":
+            description = "Destroy ground forces"
+            args = astxt(`In ${cmd.Args.Region}, for ${cmd.Args.Coalition}: ${cmd.Args.Amount}`)
+            break
+        case "MoveGroundForces":
+            description = "Move ground forces"
+            args = astxt(`From ${cmd.Args.Start} into ${cmd.Args.Destination}, for ${cmd.Args.Coalition}: ${cmd.Args.Amount}`)
+            break
+        case "RemovePlane":
+            description = "Remove plane"
+            args = astxt(`From ${cmd.Args.Airfield}, plane ${cmd.Args.Plane}: ${cmd.Args.Amount}`)
+            break
+        case "AddPlane":
+            description = "Add plane"
+            args = astxt(`From ${cmd.Args.Airfield}, plane ${cmd.Args.Plane}: ${cmd.Args.Amount}`)
+            break
+        case "RepairBuildingPart":
+            description = "Repair building"
+            args = astxt(`At ${cmd.Args.BuildingAt.Position.X}, ${cmd.Args.BuildingAt.Position.Y}, part ${cmd.Args.Part}: ${cmd.Args.Repair}`)
+            break
+        case "SetRegionOwner":
+            description = "Take over region"
+            args = astxt(`${cmd.Args.Region} by ${cmd.Args.Coalition}`)
+            break
+        case "AdvanceTime":
+            description = "Advance time"
+            args = astxt(`Hours: ${cmd.Args.Hours}`)
+            break
+        default:
+            description = cmd.Verb
+            args = document.createTextNode("")
+    }
+    const div = document.createElement("div")
     const p = document.createElement("p")
-    const txt = document.createTextNode(cmd.Verb)
+    const txt = document.createTextNode(description)
     p.appendChild(txt)
-    return p
+    div.appendChild(p)
+    div.appendChild(args)
+    return div
 }
 
-interface Result {
-    ChangeDescription: string
-    Values: Record<string, ValueType>
+interface UpdatedStorageValues {
+    BuildingAt: OrientedPosition
+    Amount: number
 }
+
+interface UpdatedStorageResult {
+    ChangeDescription: "UpdatedStorageValue"
+    Values: UpdatedStorageValues
+}
+
+interface UpdatedPlanesValues {
+    Airfield: string
+    Planes: Dict<number>
+}
+
+interface UpdatedPlanesResult {
+    ChangeDescription: "UpdatedPlanesAtAirfield"
+    Values: UpdatedPlanesValues
+}
+
+interface UpdatedGroundForcesValues {
+    Region: string
+    Coalition: Coalition
+    Forces: number
+}
+
+interface UpdatedGroundForcesResult {
+    ChangeDescription: "UpdatedGroundForces"
+    Values: UpdatedGroundForcesValues
+}
+
+interface RegionOwnerSetValues {
+    Region: string
+    Coalition: Coalition
+}
+
+interface RegionOwnerSetResult {
+    ChangeDescription: "RegionOwnerSet"
+    Values: RegionOwnerSetValues
+}
+
+interface TimeSetValues {
+    DateTime: DateTime
+}
+
+interface TimeSetResult {
+    ChangeDescription: "TimeSet"
+    Values: TimeSetValues
+}
+
+interface OtherResult {
+    ChangeDescription: string
+    Values: any
+}
+
+type Result = UpdatedStorageResult | UpdatedPlanesResult | UpdatedGroundForcesResult | RegionOwnerSetResult | TimeSetResult | OtherResult
 
 function resultToHtml(result: Result) {
     const li = document.createElement("li")
-    const txt = document.createTextNode(result.ChangeDescription)
-    li.appendChild(txt)
+    let txt = undefined
+    let args = undefined
+    function astxt(s: string) { return document.createTextNode(s) }
+    switch (result.ChangeDescription) {
+        case "RegionOwnerSet":
+            txt = "Region owner set"
+            args = astxt(`${result.Values.Region} now owned by ${result.Values.Coalition}`)
+            break
+        case "TimeSet":
+            txt = "Time has been set"
+            args = astxt(`Time: ${dateToStr(result.Values.DateTime)}`)
+            break
+        case "UpdatedGroundForces":
+            txt = "Ground forces amount changed"
+            args = astxt(`${result.Values.Coalition} in ${result.Values.Region} now has ${result.Values.Forces}`)
+            break
+        case "UpdatedPlanesAtAirfield":
+            txt = "Planes at airfield changed"
+            args = document.createElement("li")
+            for (const key of keysOf(result.Values.Planes)) {
+                const ul = document.createElement("ul")
+                ul.appendChild(astxt(`${key}: ${result.Values.Planes[key]}`))
+                args.appendChild(ul)
+            }
+            break
+        case "UpdatedStorageValue":
+            txt = "Storage capacity updated"
+            args = astxt(`At ${result.Values.BuildingAt.Position.X}, ${result.Values.BuildingAt.Position.Y}: ${result.Values.Amount}`)
+            break
+        default:
+            txt = result.ChangeDescription
+            args = astxt("")
+    }
+    const p = document.createElement("p")
+    p.appendChild(document.createTextNode(txt))
+    li.appendChild(p)
+    li.appendChild(args)
     return li
 }
 
@@ -399,6 +608,7 @@ function fetchDayData(world: World, idx: number) {
                                 small.appendChild(commandToHtml(cmd))
                             }
                             if (action.Results.length > 0) {
+                                // Show detailed properties for the results of the select event
                                 const ul = document.createElement("ul")
                                 ul.setAttribute("class", "list-group")
                                 for (const result of action.Results) {
