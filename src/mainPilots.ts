26ffaeee22664d7c752f79d3c27bf7b51e936a87
Johann Deneux <johann.deneux@gmail.com>
2020-08-10 11:19:52 +0200

    Add page for pilots

---
+++ src/mainPilots.ts
@@ -0,0 +1,9 @@
+/// <reference types="bootstrap" />
+/// <reference path="./config.ts" />
+/// <reference path="./util.ts" />
+/// <reference path="./types.ts" />
+/// <reference path="./dataSource.ts" />
+/// <reference path="./sampleData.ts" />
+
+const tablePilots = document.getElementById("table-pilots")
+
